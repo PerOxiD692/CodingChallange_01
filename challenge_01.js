8a2b0985786d0b2cb7ec981618c2ef8bbfdf418c
PerOxiD692 <jan-daniel.martin@gmx.de>
2021-05-18 15:40:22 +0200

    Lösung ++

--- challenge_01.js
+++ challenge_01.js
@@ -1,10 +1,13 @@
 
 /*********   CODE CHALLENGE 1 **********/
  
-/*
-Geben Sie über eine Prompt-Anforderung eine Zahl ein
-Geben Sie über eine Prompt-Anforderung eine zweite Zahl ein
-Berechnen Sie die Summe der Zahlen.
-Geben Sie die Summe der Zahlen in die Konsole aus:
-‘Die Summe der Zahlen ist: summe ‘
-*/
+
+//Geben Sie über eine Prompt-Anforderung eine Zahl ein
+let firstDigit = prompt("Bitte geben sie die erste Zahl ein:");
+//Geben Sie über eine Prompt-Anforderung eine zweite Zahl ein
+let secondDigit = prompt("Bitte geben sie die zweite Zahl ein:");
+//Berechnen Sie die Summe der Zahlen.
+let sum = (Number(firstDigit) + Number(secondDigit));
+//Geben Sie die Summe der Zahlen in die Konsole aus:
+//‘Die Summe der Zahlen ist: summe ‘
+console.log ("Die Summe der Zahlen ist: " + sum);
